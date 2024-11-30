f1fbd1b6613b71226b01a1b80c26eca41ee68d74
Vanesa Acevedo Grajales <nesa6423@gmail.com>
2024-11-29 20:25:14 -0500

    Inicio código, solicitud de datos por pantalla

---
+++ PARCIAL_FINAL/PARCIAL_FINAL/Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Ingrese el número de filas (N):");
+        int N = int.Parse(Console.ReadLine());
+        Console.WriteLine("Ingrese el número de columnas (M):");
+        int M = int.Parse(Console.ReadLine());
+
+        
+
+    }
+}
